1b188f5766d9ce14f40892510795b45cb81a6abf
Gautham Elango <43374544+gauthamdaze@users.noreply.github.com>
2019-11-15 23:04:12 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,66 +1,101 @@
 var feedList = document.querySelector('#feed');
+var searchBar = document.getElementById('searchBar');
+//default search 
+text ='India';
 
 // Data fetch
-var loadMore = function() {
+var loadMore = function(text) {
     var xhttp;
+    var searchString = text;
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            console.log(this.responseText);
             let res = JSON.parse(this.responseText);
-            addtToList(res);
+            if(searchString != 'india'){
+            addtToList(res,true,searchString);
+            }
+            else{
+                addtToList(res,false);
+            }
         }
     };
-    xhttp.open("GET", 'https://newsapi.org/v2/top-headlines?' +
-        'sources=bbc-news&' +
-        'apiKey=bc1efba457af4251b36161ca5f24d779', true);
+    xhttp.open("GET", 'https://newsapi.org/v2/everything?' +
+    'q='+searchString+'&'+
+    'from=2019-11-09&' +
+    'sortBy=popularity&' +
+    'apiKey=bc1efba457af4251b36161ca5f24d779');
     xhttp.send();
-
+    event.preventDefault();
 
 }
 
 //search Event handling
 
-const input = document.querySelector('#searchBar');
+const input = document.querySelector('#searchIcon');
 var list = document.getElementsByTagName('li');
 
-input.addEventListener('keyup', function() {
-    var text = this.value;
-
-    for (var i = 0; i < list.length; i++) {
-        if (!list[i].innerText.toUpperCase().includes(text.toUpperCase())) {
-            list[i].style.display = 'none';
-        } else {
-            list[i].style.display = 'block';
-        }
-    }
-
+input.addEventListener('click', function() {
+     text = searchBar.value;
+    loadMore(text);
 });
 
-var addtToList = function(items) {
-    for (var i = 0; i < items.totalResults; i++) {
-        var item = document.createElement('li');
-        var img = document.createElement("IMG");
-        var headlineLink = document.createElement('a');
-        img.setAttribute("src", items.articles[i].urlToImage);
-        img.setAttribute("width", "70");
-        img.setAttribute("height", "auto");
-        headlineLink.setAttribute('href', items.articles[i].title);
-        headlineLink.innerHTML = items.articles[i].title;
-        item.appendChild(img);
-        item.appendChild(headlineLink);
-        feedList.appendChild(item);
+searchBar.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        text = searchBar.value;
+    loadMore(text);
     }
+});
+
+//***************************** Feed List ************************************//
+
+var addtToList = function(items,searchmade,searchString) {
+  // if search is made remove the exisiting feed and append new feed
+        if(searchmade){
+            var myList = document.getElementById('feed');
+            myList.innerHTML = '';
+            searchBar.value = searchString;
+        }
+        for (var i = 0; i < items.articles.length; i++) {
+            var item = document.createElement('li');
+            var img = document.createElement("IMG");
+            var headlineLink = document.createElement('a');
+            img.setAttribute("src", items.articles[i].urlToImage);
+            img.setAttribute("width", "70");
+            img.setAttribute("height", "auto");
+            headlineLink.setAttribute('href', items.articles[i].title);
+            headlineLink.innerHTML = items.articles[i].title;
+            item.appendChild(img);
+            item.appendChild(headlineLink);
+            feedList.appendChild(item);
+        }
 }
 
 // Detect when scrolled to bottom.
 
 feedList.addEventListener('scroll', function() {
     if (feedList.scrollTop + feedList.clientHeight >= feedList.scrollHeight) {
-        loadMore();
+        loadMore(text);
     }
 });
 
+// nav bar 
 
-// Initially load some feed.
-loadMore();
+var sportTab  = document.querySelector('#sports');
+sportTab.addEventListener('click',function(){
+    loadMore('Sports');
+})
+var eduTab =document.querySelector('#education').addEventListener('click',function(){
+    loadMore('Education');
+})
+var techTab =document.querySelector('#tech').addEventListener('click',function(){
+    loadMore('Technology');
+})
+var polTab = document.querySelector('#politics').addEventListener('click',function(){
+    loadMore('Politics');
+})
+
+
+//Initially load some feed.
+window.onload = function() {
+    loadMore(text);
+  };
